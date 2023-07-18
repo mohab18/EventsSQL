b1e5a847c25d557fee44af291c00e40a56ed1ebf
mohab18 <olayanmohab@gmail.com>
2023-07-18 21:13:02 +0300

    Events sql file  added

---
+++ Events.sql
@@ -0,0 +1,10 @@
+﻿create Database EventsDatabase
+
+CREATE TABLE Events (
+    Id INT PRIMARY KEY IDENTITY,
+    Name VARCHAR(60) NOT NULL,
+    Location VARCHAR(60) NOT NULL,
+    Date DATETIME NOT NULL
+);
+
+go
